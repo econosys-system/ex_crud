5ee056b3458f3879300f091649a5c0aaa89cf2ab
kato <econosys.system@gmail.com>
2017-02-14 22:22:04 +0900

    [fix] bug-fix

---
+++ codeigniter/application/language/english/excrud_lang.php
@@ -0,0 +1,149 @@
+<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+/**
+* Name:  excrud Lang - English
+*
+* Author: econosys system
+* 		    http://econosys-system.com/
+**
+*/
+
+// button
+$lang['button_edit']   = 'edit';
+$lang['button_delete'] = 'delete';
+$lang['button_create'] = 'Insert a row';
+
+
+//table_title
+$lang['table_title_edit']   = 'edit';
+$lang['table_title_edit']   = 'edit';
+$lang['table_title_nodata']   = 'data empty';
+
+
+$lang['table_sql_table_definition']   = 'Table Definition';
+
+
+$lang['table_sql_query']   = 'SQL Query';
+
+$lang['table_sql_template']   = 'SQL Query template : ';
+
+$lang['table_sql_template_title00']   = '----- Select Template -----';
+
+$lang['table_sql_template_title01']   = 'SELECT DATA';
+
+$lang['table_sql_template_title02']   = 'SELECT COUNT';
+
+$lang['table_sql_template_title03']   = 'DELETE ALL ROWS AND SET AUTO_INCREMENT 0';
+
+
+
+$lang['table_create_list_from_result']   = 'Create list from result';
+
+
+
+
+$lang['table_title_table']   = 'table';
+
+$lang['table_title_records_head']   = '';
+
+$lang['table_title_records']   = 'Record(s)';
+
+$lang['table_title_select_table']   = 'Please select database table.';
+
+$lang['table_title_query_result']   = 'Result';
+
+$lang['table_title_multiple_edit']   = 'Edit data and push submit button.';
+
+$lang['table_title_multiple_no_edit_flag']   = 'Please set override.json "multiple_edit_flag" "1"';
+
+$lang['table_title_multiple_view_table']   = 'Rows';
+
+
+$lang['iput_multiple_placeholder']   = 'If you want change , edit this.';
+
+
+
+
+// html_title
+$lang['html_title_table']   = 'Table View';
+
+
+$lang['html_title_left_parenthesis']   = '[';
+
+$lang['html_title_right_parenthesis']   = ']';
+
+
+$lang['html_title_method_admintop']   = 'EXCRUD Top Page';
+
+$lang['html_title_method_search']   = 'Search';
+
+$lang['html_title_method_add']   = 'Insert a row';
+
+$lang['html_title_method_edit']   = 'Edit a row';
+
+$lang['html_title_method_delete']   = 'Delete a row';
+
+$lang['html_title_method_submit_sql']   = 'Execute Query';
+
+$lang['html_title_multiple_edit']   = 'Edit selected rows';
+
+// button
+$lang['button_data_add']   = 'Insert a row';
+
+$lang['button_multiple_edit']   = 'Edit selected rows';
+
+$lang['button_multiple_delete']  = 'Delete selected rows';
+
+$lang['button_submit']   = 'submit';
+
+$lang['button_delete']   = 'delete';
+
+$lang['button_cancel']   = 'cancel';
+
+$lang['button_submit_loading']   = 'loading ...';
+
+$lang['button_sql_submit']   = 'Execute Query';
+
+$lang['button_sql_submit_notice']   = 'Execute Query. OK ?';
+
+
+
+
+//header
+$lang['header_config']   = 'Config';
+
+$lang['header_json_sample_create']   = 'Create Json Sample';
+
+$lang['header_json_sample_create_confirm']   = "Create sample file : __sample__override.json \n OK ?";
+
+$lang['header_json_recreate']   = 'Json re-create';
+
+$lang['header_json_recreate_confirm']   = 'recreate json file. OK ?';
+
+
+
+
+$lang['header_help']   = 'Help';
+
+
+
+$lang['header_tables']   = 'Tables';
+
+$lang['header_sql']   = 'SQL Query';
+
+
+$lang['header_search_placeholder']   = 'Search';
+
+
+// JavaScript
+$lang['ja_dl_delete_submit_message'] = 'Delete selected rows. OK?';
+
+
+
+
+
+
+
+
+
+
+
---
+++ codeigniter/application/language/japanese/excrud_lang.php
@@ -0,0 +1,149 @@
+<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+/**
+* Name:  excrud Lang - 日本語
+*
+* Author: econosys system
+* 		    http://econosys-system.com/
+**
+*/
+
+// button
+$lang['button_edit']   = '編集';
+$lang['button_delete'] = '削除';
+$lang['button_create'] = 'データ新規作成';
+
+
+//table_title
+$lang['table_title_edit']   = '編集';
+$lang['table_title_edit']   = '編集';
+$lang['table_title_nodata']   = 'データがありません';
+
+
+$lang['table_sql_table_definition']   = 'テーブル定義';
+
+
+$lang['table_sql_query']   = 'SQL文';
+
+$lang['table_sql_template']   = 'SQL文テンプレート : ';
+
+$lang['table_sql_template_title00']   = '----- 選択してください -----';
+
+$lang['table_sql_template_title01']   = 'データを100件SELECT';
+
+$lang['table_sql_template_title02']   = 'データ件数を取得';
+
+$lang['table_sql_template_title03']   = '全データ削除して AUTO INCREMENT を 1にする';
+
+
+
+$lang['table_create_list_from_result']   = '検索結果からリストを作成';
+
+
+
+
+$lang['table_title_table']   = 'テーブル';
+
+$lang['table_title_records_head']   = '合計';
+
+$lang['table_title_records']   = '件';
+
+$lang['table_title_select_table']   = 'テーブルを選択してください';
+
+$lang['table_title_query_result']   = '実行結果';
+
+$lang['table_title_multiple_edit']   = '一括で変更したい項目を入力して【送信】ボタンを押してください。';
+
+$lang['table_title_multiple_no_edit_flag']   = '一括編集できる項目がありません。override.jsonから一括編集したいカラムののmultiple_edit_flagを1にしてください。';
+
+$lang['table_title_multiple_view_table']   = '対象となるデータ（このデータを一括編集します）';
+
+
+$lang['iput_multiple_placeholder']   = '変更する場合のみ入力してください';
+
+
+
+
+// html_title
+$lang['html_title_table']   = 'テーブル';
+
+
+$lang['html_title_left_parenthesis']   = '『';
+
+$lang['html_title_right_parenthesis']   = '』';
+
+
+$lang['html_title_method_admintop']   = '管理トップ';
+
+$lang['html_title_method_search']   = 'データ検索';
+
+$lang['html_title_method_add']   = 'データ新規作成';
+
+$lang['html_title_method_edit']   = 'データ編集';
+
+$lang['html_title_method_delete']   = 'データ削除';
+
+$lang['html_title_method_submit_sql']   = 'SQL文の実行';
+
+$lang['html_title_multiple_edit']   = 'データの一括編集';
+
+// button
+$lang['button_data_add']   = 'データ新規作成';
+
+$lang['button_multiple_edit']   = 'データ一括編集';
+
+$lang['button_multiple_delete']  = 'データ一削除';
+
+$lang['button_submit']   = '送信';
+
+$lang['button_delete']   = '削除';
+
+$lang['button_cancel']   = 'キャンセル';
+
+$lang['button_submit_loading']   = 'アクセス中 ...';
+
+$lang['button_sql_submit']   = 'SQL文の実行';
+
+$lang['button_sql_submit_notice']   = 'SQL文を実行します。この処理は取り消すことができません。よろしいですか？';
+
+
+
+
+//header
+$lang['header_config']   = '設定';
+
+$lang['header_json_sample_create']   = 'Json サンプル作成';
+
+$lang['header_json_sample_create_confirm']   = '__sample__override.jsonファイルを自動生成します。よろしいですか？';
+
+$lang['header_json_recreate']   = 'Json 再作成';
+
+$lang['header_json_recreate_confirm']   = 'jsonファイルを再生成します。よろしいですか？';
+
+
+
+
+$lang['header_help']   = 'ヘルプ';
+
+
+
+$lang['header_tables']   = 'テーブル';
+
+$lang['header_sql']   = 'SQL実行';
+
+
+$lang['header_search_placeholder']   = '検索';
+
+
+// JavaScript
+$lang['ja_dl_delete_submit_message'] = '選択したデータを削除します。よろしいですか？';
+
+
+
+
+
+
+
+
+
+
+
